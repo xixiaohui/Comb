a069a704404d59dbd8d08806f6e5a6596c56da4e
xixiaohui <yamazake@yeah.net>
2017-04-19 13:12:32 +0800

    add Comb first index page

---
+++ Comb/views.py
@@ -0,0 +1,16 @@
+# from django.shortcuts import render
+
+
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+"""
+	主页面
+"""
+def index(request):
+	return HttpResponse("Hello,Comb!")
+
+
+	
